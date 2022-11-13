c25079828ad95c0b712c176bc188c40f59fce717
Roman Polikutkin <Roman_Polikutkin@epam.com>
2022-11-13 17:05:51 +0300

    service-bus: remove unnecessary logic which supposed to be done by service-bus

--- eShopOnWeb/src/OrderItemsReserver/ReserveOrderItemFunction.cs
+++ eShopOnWeb/src/OrderItemsReserver/ReserveOrderItemFunction.cs
@@ -17,64 +17,32 @@ namespace OrderItemsReserver;
 
 public static class ReserveOrderItemFunction
 {
-    private const int RetryCount = 3;
-
     [FunctionName("ReserveOrderItemFunction")]
     public static async Task Run(
         [ServiceBusTrigger("send-order", Connection = "ServiceBusConnectionString")] string queueItem,
         ILogger log)
     {
-        var isSent = false;
-        var counter = 0;
-        string responseMessage = null;
-        while (!isSent && counter < RetryCount)
+        try
         {
-            try
-            {
-                string connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-                string containerName = Environment.GetEnvironmentVariable("StorageContainerName");
+            string connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = Environment.GetEnvironmentVariable("StorageContainerName");
 
-                var orderRequest = JsonConvert.DeserializeObject<OrderRequest>(queueItem);
-                var name = $"OrderRequest-{orderRequest.Id}.json";
+            var orderRequest = JsonConvert.DeserializeObject<OrderRequest>(queueItem);
+            var name = $"OrderRequest-{orderRequest.Id}.json";
 
-                var blobClient = new BlobContainerClient(connection, containerName);
-                var blob = blobClient.GetBlobClient(name);
+            var blobClient = new BlobContainerClient(connection, containerName);
+            var blob = blobClient.GetBlobClient(name);
 
-                var stream = GenerateStreamFromString(queueItem);
+            var stream = GenerateStreamFromString(queueItem);
 
-                await blob.UploadAsync(stream);
+            await blob.UploadAsync(stream);
 
-                log.LogInformation($"C# ServiceBus queue trigger function processed message: {queueItem}");
-                responseMessage = $"ServiceBus triggered ReserveOrderItemFunction executed successfully. {name} was uploaded to blob storage";
-                isSent = true;
-            }
-            catch (Exception e)
-            {
-                log.LogError(e, "Exception occured during ReserveOrderItemFunction request processing");
-            }
-            
-            counter++;
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {queueItem}");
         }
-
-        if (!isSent)
+        catch (Exception e)
         {
-            string logicAppsUri = Environment.GetEnvironmentVariable("LogicAppsUri");
-            var email = new
-            {
-                to = "roman_polikutkin@epam.com",
-                subject = "saving order request failed",
-                body = $@"Dear admin, <br/> saving order request failed. <br/> Request info: {queueItem}"
-            };
-
-            using var client = new HttpClient();
-            var strContent = JsonConvert.SerializeObject(email);
-            var content = new StringContent(strContent);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            var result = await client.PostAsync(logicAppsUri, content);
-            string resultContent = await result.Content.ReadAsStringAsync();
-
-            log.LogInformation($"email with error was sent to admin group");
+            log.LogError(e, "Exception occured during ReserveOrderItemFunction request processing");
+            throw;
         }
     }
 
