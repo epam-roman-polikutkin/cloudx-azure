5ff0bd2180592f7307f73bce5db984cc709194e8
Roman Polikutkin <Roman_Polikutkin@epam.com>
2022-11-20 18:03:00 +0300

    last-task: add logic

--- eShopOnWeb/src/ApplicationCore/Services/RequestService.cs
+++ eShopOnWeb/src/ApplicationCore/Services/RequestService.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using BlazorShared;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.Requests;
@@ -11,6 +12,7 @@ using Newtonsoft.Json;
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 public class RequestService : IRequestService
 {
+    private const string QueueName = "send-order";
     private readonly BaseUrlConfiguration _baseUrlConfig;
     private readonly IAppLogger<Order> _appLogger;
 
@@ -29,18 +31,12 @@ public class RequestService : IRequestService
 
         var orderRequest = GetOrderRequest(order);
 
-        using (var client = new HttpClient())
-        {
-            var strContent = JsonConvert.SerializeObject(orderRequest);
-            var content = new StringContent(strContent);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        var client = new ServiceBusClient(_baseUrlConfig.ReserveOrderItemFunctionBaseUrl);
+        var sender = client.CreateSender(QueueName);
+        var message = new ServiceBusMessage(JsonConvert.SerializeObject(orderRequest));
+        await sender.SendMessageAsync(message);
 
-            var result = await client.PostAsync(
-                $"{_baseUrlConfig.ReserveOrderItemFunctionBaseUrl}?orderId={order.Id}", content);
-            string resultContent = await result.Content.ReadAsStringAsync();
-
-            _appLogger.LogInformation(resultContent);
-        }
+        _appLogger.LogInformation($"Order request with orderId={order.Id} was sent to service bus queue {QueueName}");
     }
 
     public async Task SendDeliveryProcessOrderRequestAsync(Order order)
--- eShopOnWeb/src/OrderItemsReserver/ReserveOrderItemFunction.cs
+++ eShopOnWeb/src/OrderItemsReserver/ReserveOrderItemFunction.cs
@@ -6,40 +6,54 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using Microsoft.eShopWeb.ApplicationCore.Entities.Requests;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace OrderItemsReserver;
 
 public static class ReserveOrderItemFunction
 {
     [FunctionName("ReserveOrderItemFunction")]
-    public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+    public static async Task Run(
+        [ServiceBusTrigger("send-order", Connection = "ServiceBusConnectionString")] string queueItem,
         ILogger log)
     {
         try
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
             string containerName = Environment.GetEnvironmentVariable("StorageContainerName");
 
-            var orderId = req.Query["orderId"];
-            var name = $"OrderRequest-{orderId}.json";
+            var orderRequest = JsonConvert.DeserializeObject<OrderRequest>(queueItem);
+            var name = $"OrderRequest-{orderRequest.Id}.json";
 
             var blobClient = new BlobContainerClient(connection, containerName);
             var blob = blobClient.GetBlobClient(name);
 
-            await blob.UploadAsync(req.Body);
+            var stream = GenerateStreamFromString(queueItem);
 
-            string responseMessage = $"HTTP triggered ReserveOrderItemFunction executed successfully. {name} was uploaded to blob storage";
+            await blob.UploadAsync(stream);
 
-            return new OkObjectResult(responseMessage);
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {queueItem}");
         }
         catch (Exception e)
         {
             log.LogError(e, "Exception occured during ReserveOrderItemFunction request processing");
-
-            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            throw;
         }
+    }
+
+    public static Stream GenerateStreamFromString(string s)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+
+        return stream;
     }
 }
